c847cc3971038f8a15b1c5da8a3ac023eab66bd1
PGee <283959070@qq.com>
2022-11-04 16:38:07 +0800

    fix(custom): fixed build script

--- scripts/package-playable.sh
+++ scripts/package-playable.sh
@@ -9,19 +9,23 @@ PLUGIN_ROOT="${pwd}/packages/playable-ads-adpter"
 
 PLUGIN_DIST="${PLUGIN_ROOT}/dist"
 
-cd $PLUGIN_DIST
-
 echo "Package 3.x Plugin..."
 
 npm run build -- --environment BUILD_VERSION:3x
+
+cd $PLUGIN_DIST
 
 zip -r -v -9 playable-36x.zip ./playable-ads-adapter
 
 echo "Package 3.x Plugin Finished."
 
+cd PLUGIN_ROOT
+
 echo "Package 2.4.x Plugin..."
 
 npm run build -- --environment BUILD_VERSION:2x
+
+cd $PLUGIN_DIST
 
 zip -r -v -9 playable-24x.zip ./playable-ads-adapter
 
