4c79a381af167c82d85f8c5491ecd33e015e215a
PGee <283959070@qq.com>
2022-11-04 16:43:32 +0800

    feat(custom): update build script

--- scripts/package-playable.sh
+++ scripts/package-playable.sh
@@ -3,9 +3,7 @@ set -e
 
 pwd=$(pwd)
 
-PROJECT_ROOT=$pwd
-
-PLUGIN_ROOT="${pwd}/packages/playable-ads-adpter"
+PLUGIN_ROOT="/packages/playable-ads-adpter"
 
 PLUGIN_DIST="${PLUGIN_ROOT}/dist"
 
@@ -21,7 +19,7 @@ zip -r -v -9 playable-36x.zip ./playable-ads-adapter
 
 echo "Package 3.x Plugin Finished."
 
-cd PLUGIN_ROOT
+cd ..
 
 echo "Package 2.4.x Plugin..."
 
