e9e825e7c10808b0a367c8579f98bfbf481d7fe9
PGee <283959070@qq.com>
2022-11-22 17:31:35 +0800

    docs(custom): update docs

--- .cz-config.js
+++ .cz-config.js
@@ -1,29 +1,28 @@
 module.exports = {
   types: [
-    { value: 'feat', name: 'feat:     A new feature' },
-    { value: 'fix', name: 'fix:      A bug fix' },
+    { value: 'feat', name: '✅ feat:     A new feature' },
+    { value: 'fix', name: '🐛 fix:      A bug fix' },
     {
       value: 'style',
       name:
-        'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
+        '🎨 style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)',
     },
     {
       value: 'refactor',
       name:
-        'refactor: A code change that neither fixes a bug nor adds a feature',
+        '✨ refactor: A code change that neither fixes a bug nor adds a feature',
     },
-    { value: 'revert', name: 'revert:   Revert to a commit' },
+    { value: 'revert', name: '🪃 revert:   Revert to a commit' },
     {
       value: 'chore',
       name:
-        'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
+        '🖇️ chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation',
     },
-    { value: 'docs', name: 'docs:     Documentation only changes' },
+    { value: 'docs', name: '📝 docs:     Documentation only changes' },
     {
       value: 'perf',
-      name: 'perf:     A code change that improves performance',
+      name: '🚀 perf:     A code change that improves performance',
     },
-    { value: 'test', name: 'test:     Adding missing tests' },
   ],
 
   scopes: [
